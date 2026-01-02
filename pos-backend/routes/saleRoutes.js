f201d4dd88041fa02ab39f62ebcaf6341394caea
ihabhaydaw1 <ihabhaydaw@gmail.com>
2026-01-03 00:39:35 +0200

    fixed some potential route issues

--- pos-backend/routes/saleRoutes.js
+++ pos-backend/routes/saleRoutes.js
@@ -2,13 +2,14 @@ const router = require("express").Router();
 const c = require("../controllers/saleController");
 
 // CRUD
+// Aggregate example
+router.get("/aggregate/products", c.salesByProduct);
+
+// CRUD
 router.post("/", c.createSale);
 router.get("/", c.getSales);
 router.get("/:id", c.getSaleById);
 router.put("/:id", c.updateSale);
 router.delete("/:id", c.deleteSale);
-
-// Aggregate example
-router.get("/aggregate/products", c.salesByProduct);
 
 module.exports = router;
