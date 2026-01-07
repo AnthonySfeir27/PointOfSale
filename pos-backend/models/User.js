97c7d7ab210b58df87cb3191afdb4c41c81d18ec
ihabhaydaw1 <ihabhaydaw@gmail.com>
2026-01-07 10:21:11 +0200

    added validation to user

--- pos-backend/models/User.js
+++ pos-backend/models/User.js
@@ -2,7 +2,18 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, lowercase: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function (v) {
+        return /^\S+$/.test(v);
+      },
+      message: props => `${props.value} should not contain spaces!`
+    }
+  },
   role: { type: String, enum: ["admin", "cashier"], required: true },
   password: { type: String, required: true, minlength: 6 },
   createdAt: { type: Date, default: Date.now }
